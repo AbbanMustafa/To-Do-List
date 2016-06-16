bfc87cbd509f0f79477a69949f7f31132be949e6
MustafaAbban <mustafaabban@utexas.edu>
2016-06-16 16:04:30 -0500

    update three

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,30 @@
 import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
 import './body.html';
 
-
 Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
 
-  tasks: [
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
 
-    { text: "Learn to Program"},
-    { text: "Become good at it"},
-    { text: "Maybe get paid to do it"},
-  ],
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  },
 });
