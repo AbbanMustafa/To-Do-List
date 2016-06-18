1481b566bbad8ed5015f710946cf1870081f2cce
MustafaAbban <mustafaabban@utexas.edu>
2016-06-18 01:40:40 -0500

    check boxes

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -13,6 +13,13 @@ Template.body.onCreated(function bodyOnCreated() {
 
 Template.body.helpers({
   tasks() {
+
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
     // Show newest tasks at the top
     return Tasks.find({}, { sort: { createdAt: -1 } });
   },
@@ -35,4 +42,7 @@ Template.body.events({
     // Clear form
     target.text.value = '';
   },
+  'change .hide-completed input'(event, instance) {
+     instance.state.set('hideCompleted', event.target.checked);
+
 });
